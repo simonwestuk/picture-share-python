2f9a3e7026cf6da16fa2042d126097d1eef9abe0
Simon West <43855704+simonwestuk@users.noreply.github.com>
2024-09-13 11:30:39 +0000

    Refactor home page title to "Welcome"

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,7 +6,7 @@ def test_home_page(test_client):
     """
     response = test_client.get('/')
     assert response.status_code == 200
-    assert b"Home Page" in response.data
+    assert b"Welcome" in response.data
     
 def test_register_page(test_client):
     """
